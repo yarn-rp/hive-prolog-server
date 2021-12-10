9d7b759dcba181558b68600bb61cf15bf24f854e
jvaldesgonzalez <jvaldesgonzalez9@gmail.com>
2021-12-09 20:56:39 -0500

    fix(routes) renew routes

--- server.pl
+++ server.pl
@@ -13,16 +13,13 @@
 :- consult(game), import(game).
 
 % --------------------------------------URL HANDLERS--------------------------------------
-:- http_handler('/hive_api/ping', handle_request_ping_pong, []).
-:- http_handler('/hive_api/insect/get_possible_placements', handle_request_get_possible_placements, []).
-:- http_handler('/hive_api/insect/get_possible_moves', handle_request_get_possible_moves, []).
-:- http_handler('/hive_api/insect/place_insect', handle_request_place_insect, []).
-:- http_handler('/hive_api/insect/move_insect', handle_request_move_insect, []).
-:- http_handler('/hive_api/insect/get_last', handle_request_get_last, []).
-:- http_handler('/hive_api/game/game_stats', handle_request_game_stats, []).
-:- http_handler('/hive_api/game/reset_game', handle_request_reset_game, []).
-:- http_handler('/hive_api/game/new_game', handle_request_new_game, []).
-:- http_handler('/hive_api/insect/queen_surrounded', handle_request_queen_surrounded, []).
+:- http_handler('/api/ping', handle_request_ping_pong, []).
+:- http_handler('/api/insect/get_possible_placements', handle_request_get_possible_placements, []).
+:- http_handler('/api/insect/get_possible_moves', handle_request_get_possible_moves, []).
+:- http_handler('/api/movePieceFromHand', handle_request_place_insect, []).
+:- http_handler('/api/movePieceFromBoard', handle_request_move_insect, []).
+:- http_handler('/api/newGame', handle_request_new_game, []).
+:- http_handler('/api/insect/queen_surrounded', handle_request_queen_surrounded, []).
 
 % get_last_insect
 % --------------------------------------METHODS--------------------------------------
@@ -94,16 +91,6 @@ moveInsect(_{type:Type, id:Id, lvl:Lvl, hexagon_ori:Hexagon_Ori, hexagon_end:Hex
     game:next_player(),
     Status_Code = 200.
 
-% get the last insect in hexagon
-getLast(_{hexagon:Hexagon}, _{status_code:Status_Code ,msg:MSG}):-
-    insects:get_last_insect(Hexagon, Insect),
-    Insect == [],
-    MSG = "Empty hexagon!",
-    Status_Code = 400,!.
-getLast(_{hexagon:Hexagon}, _{status_code:Status_Code ,insect:Insect}):-
-    insects:get_last_insect(Hexagon, Insect),
-    Status_Code = 200.
-
 % Get possible placements
 % getPossiblePlacements(_{type:_}, _{status_code:Status_Code, msg:MSG}):-
 %     game:current_player(Player_id),
@@ -112,6 +99,7 @@ getLast(_{hexagon:Hexagon}, _{status_code:Status_Code ,insect:Insect}):-
 %     string_concat(Name, " cannot play and needs to pass its turn.", MSG),
 %     Status_Code = 401,
 %     game:next_player(),!.
+
 getPossiblePlacements(_{type:_}, _{status_code:Status_Code, msg:MSG}):-
     game:current_player(Player_id),
     game:player(Player_id, _, Number_of_moves, _, _, _),
@@ -191,10 +179,6 @@ newGame(_{mode:Mode, level:Level}, _{msg:MSG}):-
     game:new_game(ModeAtom, Level, MSG).
 
 % --------------------------------------Request Handlers--------------------------------------
-% Handle ping pong
-handle_request_ping_pong(_) :-
-    ping_pong(Res),
-    reply_json_dict(Res).
 
 % Handle place insect
 handle_request_place_insect(Req) :-
@@ -220,60 +204,9 @@ handle_request_get_possible_moves(Req):-
     getPossibleMoves(Query, Res),
     reply_json_dict(Res).
 
-% handle get last insect in hexagon
-handle_request_get_last(Req):-
-    http_read_json_dict(Req, Query),
-    getLast(Query, Res),
-    reply_json_dict(Res).
-
-% Handle game stats
-handle_request_game_stats(_):-
-    game:current_player(Current_player_id),
-    game:player(p1, Name_p1, Number_of_moves_p1, Queen_bee_placed_p1, Type_player1, Game_over1),
-    game:player(p2, Name_p2, Number_of_moves_p2, Queen_bee_placed_p2, Type_player2, Game_over2),
-
-    game:insects:all_insects(_, _, p1, _, false,_, Non_placed_insects_p1),
-    game:insects:all_insects(_, _, p2, _, false,_, Non_placed_insects_p2),
-
-    game:insects:all_insects(_, _, _, _, true,_,Placed_insects),
-    
-
-    Players_info =
-    _{
-        p1:_{
-            id:p1, name:Name_p1,    
-            number_of_moves:Number_of_moves_p1,
-            queen_bee_placed:Queen_bee_placed_p1,
-            non_placed_insects: Non_placed_insects_p1,
-            type_player: Type_player1,
-            game_over: Game_over1
-            },
-        p2:_{
-            id:p2, name:Name_p2,
-            number_of_moves:Number_of_moves_p2,
-            queen_bee_placed:Queen_bee_placed_p2,
-            non_placed_insects: Non_placed_insects_p2,
-            type_player: Type_player2,
-            game_over: Game_over2
-            }
-    },
-
-    Hive = Placed_insects,
-
-    Status_Code = 200,
-
-    Res = _{status_code:Status_Code, current_player_id:Current_player_id, players_info:Players_info, hive:Hive},
-    reply_json_dict(Res).
-
 % handle queen sorrounded
 handle_request_queen_surrounded(Req):-
     queenSurrounded(Res),
-    reply_json_dict(Res).
-
-% handle reset game
-handle_request_reset_game(_):-
-    game:reset_game(MSG),
-    Res = _{msg:MSG},
     reply_json_dict(Res).
 
 % handle new game
